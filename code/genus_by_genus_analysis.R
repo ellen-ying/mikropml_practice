41bd8fddae7e5232023dff3196ee56e60dc4ca07
Yurun Ying <yurunying.sh@gmail.com>
2022-05-12 10:48:40 +0800

    reorganized code to make DRY

--- code/genus_by_genus_analysis.R
+++ code/genus_by_genus_analysis.R
@@ -1,65 +1,4 @@
-library(tidyverse)
-library(here)
-library(broom)
-library(ggtext)
-library(wesanderson)
-
-set.seed(1990331)
-
-shared <- 
-  # locate the file
-  here("data/raw/baxter.subsample.shared") %>% 
-  # read file, specify the coloumn type
-  read_tsv(col_types = cols(Group = col_character(),
-                            .default = col_double())
-          ) %>% 
-  # clean the name, select the columns we want
-  rename_all(tolower) %>% 
-  select(group, starts_with("otu")) %>% 
-  # tidy up the data into a long format
-  pivot_longer(cols = -group, names_to = "otu", values_to = "count")
-
-taxonomy <- 
-  here("data/raw/baxter.cons.taxonomy") %>% 
-  read_tsv() %>% 
-  rename_all(tolower) %>% 
-  select(otu, taxonomy) %>% 
-  mutate(
-    otu = tolower(otu),
-    # replace multiple cases that match the regular expression
-    # \\d+ means matching one or more digits
-    taxonomy = str_replace_all(taxonomy, "\\(\\d+\\)", ""),
-    # replace the first case that matches the expression
-    taxonomy = str_replace(taxonomy, ";unclassified", "_unclassified"),
-    taxonomy = str_replace_all(taxonomy, ";unclassified", ""),
-    # ;$ matches the semicolon at the end of a string
-    taxonomy = str_replace_all(taxonomy, ";$", ""),
-    # .*; matches everything up to a semicolon
-    taxonomy = str_replace_all(taxonomy, ".*;", "")
-    ) 
-
-metadata <- 
-  here("data/raw/baxter.metadata.tsv") %>% 
-  read_tsv(col_types = cols(sample = col_character())) %>% 
-  rename_all(tolower) %>% 
-  mutate(
-    # two ways to doing the coding, obviously one is more efficient
-    #srn = ifelse(dx_bin %in% c("Adv Adenoma", "Cancer"), TRUE, FALSE)
-    srn = dx_bin == "Adv Adenoma" | dx_bin == "Cancer",
-    lesion = dx_bin == "Adv Adenoma" | dx_bin == "Cancer" | dx_bin == "Adenoma"
-    )
-  
-composite <- 
-  inner_join(shared, taxonomy, by = "otu") %>% 
-  # count the total number of taxonomy in each group
-  group_by(group, taxonomy) %>% 
-  summarize(count = sum(count), .groups = "drop") %>% 
-  group_by(group) %>% 
-  # sum over each group to calculate the relative abundance
-  mutate(rel_abund = count / sum(count)) %>% 
-  ungroup() %>% 
-  select(-count) %>% 
-  inner_join(., metadata, by = c("group" = "sample"))
+source("code/genus_process.R")
 
 sig_genera <- 
   composite %>% 
@@ -150,7 +89,11 @@ get_roc_data <- function(x, direction) {
   score <- x$score
   actual <- x$srn
   
+  # for each threshold value, get sensitivity and specificity
+  # output into a single tibble
   map_dfr(threshold, ~ get_sens_spec(.x, score, actual, direction)) %>% 
+    # add a row to connect the data to the upper right corner in the plot
+    # where we have specificity = 0 and sensitivity = 1
     add_row(sensitivity = 1, specificity = 0)
   
 }
@@ -163,6 +106,9 @@ roc_data <-
   select(group, taxonomy, rel_abund, fit_result, srn) %>% 
   pivot_wider(names_from = taxonomy, values_from = rel_abund) %>% 
   pivot_longer(cols = -c(group, srn), names_to = "metric", values_to = "score") %>% 
+  # by using this syntax we can get tibbles in the nested column
+  # otherwise, I don't even know what they are 
+  # and they don't seem to work with map function very well
   nest(data = -metric) %>% 
   mutate(
     direction = ifelse(metric == "Lachnospiraceae_unclassified", "lessthan", "greaterthan")
---
+++ code/genus_process.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(here)
+library(broom)
+library(ggtext)
+library(wesanderson)
+
+set.seed(1990331)
+
+shared <- 
+  # locate the file
+  here("data/raw/baxter.subsample.shared") %>% 
+  # read file, specify the coloumn type
+  read_tsv(col_types = cols(Group = col_character(),
+                            .default = col_double())
+  ) %>% 
+  # clean the name, select the columns we want
+  rename_all(tolower) %>% 
+  select(group, starts_with("otu")) %>% 
+  # tidy up the data into a long format
+  pivot_longer(cols = -group, names_to = "otu", values_to = "count")
+
+taxonomy <- 
+  here("data/raw/baxter.cons.taxonomy") %>% 
+  read_tsv() %>% 
+  rename_all(tolower) %>% 
+  select(otu, taxonomy) %>% 
+  mutate(
+    otu = tolower(otu),
+    # replace multiple cases that match the regular expression
+    # \\d+ means matching one or more digits
+    taxonomy = str_replace_all(taxonomy, "\\(\\d+\\)", ""),
+    # replace the first case that matches the expression
+    taxonomy = str_replace(taxonomy, ";unclassified", "_unclassified"),
+    taxonomy = str_replace_all(taxonomy, ";unclassified", ""),
+    # ;$ matches the semicolon at the end of a string
+    taxonomy = str_replace_all(taxonomy, ";$", ""),
+    # .*; matches everything up to a semicolon
+    taxonomy = str_replace_all(taxonomy, ".*;", "")
+  ) 
+
+metadata <- 
+  here("data/raw/baxter.metadata.tsv") %>% 
+  read_tsv(col_types = cols(sample = col_character())) %>% 
+  rename_all(tolower) %>% 
+  mutate(
+    # two ways to doing the coding, obviously one is more efficient
+    #srn = ifelse(dx_bin %in% c("Adv Adenoma", "Cancer"), TRUE, FALSE)
+    srn = dx_bin == "Adv Adenoma" | dx_bin == "Cancer",
+    lesion = dx_bin == "Adv Adenoma" | dx_bin == "Cancer" | dx_bin == "Adenoma"
+  )
+
+composite <- 
+  inner_join(shared, taxonomy, by = "otu") %>% 
+  # count the total number of taxonomy in each group
+  group_by(group, taxonomy) %>% 
+  summarize(count = sum(count), .groups = "drop") %>% 
+  group_by(group) %>% 
+  # sum over each group to calculate the relative abundance
+  mutate(rel_abund = count / sum(count)) %>% 
+  ungroup() %>% 
+  select(-count) %>% 
+  inner_join(., metadata, by = c("group" = "sample"))
