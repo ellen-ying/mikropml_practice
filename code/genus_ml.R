fa1224c316632af2fc2b6674357d3fd8f132cb63
Yurun Ying <yurunying.sh@gmail.com>
2022-05-12 12:37:32 +0800

    added code to preprocess the data and train a new model

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -11,11 +11,38 @@ srn_genus_data <-
   mutate(srn = ifelse(srn, "srn", "healthy")) %>% 
   select(srn, everything())
 
-srn_genus_results <- 
+# practice <- 
+#   composite %>% 
+#   select(group, fit_result, site, gender, srn, weight) %>% 
+#   distinct() %>% 
+#   # code 0 weight as NA value
+#   mutate(weight = na_if(weight, 0)) %>% 
+#   select(-group)
+
+# preprocessing data
+# rescaling fit_result and weight
+# remove features contains near-zero variance
+srn_genus_preprocessed <- 
+  preprocess_data(srn_genus_data, outcome_colname = "srn")$dat_transformed
+
+srn_genus_results_no_pp <- 
   run_ml(srn_genus_data,
+         # usinglogistic regression method
          method = "glmnet",
          outcome_colname = "srn",
          kfold = 5, # five-fold cross-validation
          cv_times = 100, # iteration times
          training_frac = 0.8, # training set = 80% of the dataset
          seed = 19990331)
+
+# train with preprocessed data
+srn_genus_results <- 
+  run_ml(srn_genus_preprocessed,
+         # usinglogistic regression method
+         method = "glmnet",
+         outcome_colname = "srn",
+         kfold = 5, # five-fold cross-validation
+         cv_times = 100, # iteration times
+         training_frac = 0.8, # training set = 80% of the dataset
+         seed = 19990331)
+
